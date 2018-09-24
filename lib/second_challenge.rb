aff548be8284f82fb94fde0687e30374e9112f4e
Samuel Cornet <samuel@noukod.com>
2018-09-24 14:53:24 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,14 +1,7 @@
 def second_challenge
   epic_tragedy = {
-    :montague => {
-    :patriarch => {
-      :name => "Lord Montague",
-      :age => "53"
-      },
-    :capulet => {
-    :patriarch => {
-      :name => "Lord Capulet",
-      :age => "50"
-    }}
+    :montague => {:patriarch => {},
+    :capulet => {:patriarch => {}
+  }
 
 end
